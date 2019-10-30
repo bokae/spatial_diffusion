36183d0da01133c82aabe0bc76116dea64536660
Eszter Bokanyi <bokanyi@complex.elte.hu>
2019-10-30 15:19:12 +0100

    Added comments.

--- SI.py
+++ SI.py
@@ -6,16 +6,29 @@ import sys
 
 class SI:
     def __init__(self, **conf):
+
+        # reading params from the config conf, otherwise set to default
+        # parameter 'p' of the Bass model
         self.p = conf.get("p", 0.000104)
+        # parameter 'q' of the Bass model
         self.q = conf.get("q", 0.12)
+        # number of timesteps
         self.stop = conf.get("stop", 129)
+        # correcting the threshold
         self.high = conf.get("high", 0)
         self.low = conf.get("low", 0)
+        # total number of runs with the above parameter settings
         self.num_runs = conf.get("num_runs", 1)
 
+        # reading in byte-saved variables created by preferences.py that are the same for each run
         with open('adjacency.pickle', 'rb') as f:
+            # A is the adjacency matrix
+            # indexmap maps user_ids to the row indices of A
+            # indexmap_back maps row indices of A to user_ids
+            # seed is a boolean vector of the initial infected nodes
             self.A, self.indexmap, self.indexmap_back, self.seed = pickle.load(f)
 
+        # creating the necessary variables, setting the initial infected nodes
         self.restart()
 
     def restart(self):
@@ -24,7 +37,9 @@ class SI:
 
         # creating infected and susceptible lists
         self.infected = np.zeros(self.A.shape[0], dtype=bool)
+        # initial infection
         self.infected[self.seed] = True
+        # initial susceptibles, aka everybody who's not infected
         self.susceptible = ~self.infected
 
         # storing the infection time for the nodes, setting infected nodes times to 1
@@ -32,8 +47,11 @@ class SI:
 
         # storing number of infected neighbors for nodes
         self.node_neighborhood_num_infected = np.matrix(np.zeros_like(self.infected, dtype=float))
+        # storing total number of neighbors
         self.node_neighborhood_num_neighbors = self.A.sum(axis=0)
+        # adding 1 for isolated nodes to avoid division by 0
         self.node_neighborhood_num_neighbors[(self.node_neighborhood_num_neighbors == 0)[0]] = 1
+        # counting number of infected neighbors for initial infection
         self.node_neighborhood_num_infected += self.A[self.infected].sum(axis=0)
 
     def transform(self, a):
@@ -48,33 +66,45 @@ class SI:
         return np.polyval(params, a)*np.array(a)
 
     def step_time(self):
-        # create neighbor fractions, flip coins for infection
+        # increment time counter
+        self.time_counter += 1
+
+        # fraction of infected neighbors vs total neighbors
         a = self.node_neighborhood_num_infected/self.node_neighborhood_num_neighbors
+        # uniform random number between 0 and 1
         r = np.random.rand(a.shape[1])
+        # calculating threshold, comparing random number to threshold
         mask = (r < self.p + self.q*self.transform(a))
+        # infect nodes where random number is smaller than the threshold and that are susceptible
         new_infected = np.array(self.susceptible & mask)[0]
 
-        # update lists, update counters
+        # store infection time for newly infected nodes
         self.time_infected[new_infected] = self.time_counter
+        # union of old and new infections
         self.infected = self.infected | new_infected
+        # decreasing the susceptibles with the newly infected nodes
         self.susceptible = self.susceptible & (~new_infected)
+        # incrementing the counters in the array storing the number of infected neighbors
         self.node_neighborhood_num_infected += self.A[new_infected].sum(axis=0)
 
         return new_infected.sum()
 
     def run_new(self):
+        # init simulation, time_counter = 1
         self.restart()
-        for time_counter in range(2, self.stop):
-            self.time_counter = time_counter
+        # stepping time
+        for t in range(2, self.stop):
             n = self.step_time()
 
-        # storing results
+        # storing results, raw output
         return {
             "time_infected": {str(self.indexmap_back[i]): int(t) for i, t in enumerate(self.time_infected)},
             "p": self.p,
             "q": self.q,
             "stop": self.stop,
-            "num_nodes": self.A.shape[0]
+            "num_nodes": self.A.shape[0],
+            "high": self.high,
+            "low": self.low
         }
 
 
